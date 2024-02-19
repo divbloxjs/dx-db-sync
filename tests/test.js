7ba848274eb34de992d86f68b27424a1796c4787
Dani <dani.simeonov@stratusolve.com>
2024-02-19 14:51:39 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,5 @@
 import { syncDatabase } from "../index.js";
 
-// import databaseConfig from "./database-config.json" with { type: 'json' };
-// import dataModel from "./example-data-model.json" with { type: 'json' };
-
 import databaseConfig from "./database-config.json" assert { type: "json" };
 import dataModel from "./example-data-model.json" assert { type: "json" };
 
@@ -10,7 +7,7 @@ await syncDatabase(
     {
         databaseCaseImplementation: "snakecase",
         databaseConfig: databaseConfig,
-        dataModelPath: "./tests/example-data-model.json",
+        dataModelPath: dataModel,
     },
     false,
 );
